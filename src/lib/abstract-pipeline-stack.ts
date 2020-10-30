41a7546eaabb27c1123e978be728a0c4d1b6112d
Omar <omar.kacimi@otto.de>
2020-10-30 17:27:25 +0100

    upgraded to latest aws-cdk-github-pipeline

--- src/lib/abstract-pipeline-stack.ts
+++ src/lib/abstract-pipeline-stack.ts
@@ -2,7 +2,7 @@
 import {App, Stack, StackProps} from "@aws-cdk/core/lib";
 import {AwsInfrastructurePipeline} from "./pipeline";
 
-export class AwsInfrastructureStack extends Stack {
+export class AbstractPipelineStack extends Stack {
     constructor(scope: App, id: string, props?: StackProps) {
         super(scope, id, props)
         new AwsInfrastructurePipeline(scope, this, id + "-pipeline", props)
